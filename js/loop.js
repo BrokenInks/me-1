aa7c11cb1244ab703b32862f702c2f24337ed3fc
ZippWitch-alt <78442160+ZippWitch-alt@users.noreply.github.com>
2021-05-28 23:28:11 +0300

    a

--- js/loop.js
+++ js/loop.js
@@ -1,14 +1,17 @@
 const profile = {
+    defColor: '#7f78bd',
     1: {
         name: 'New Senpaii#0209',
-        description: 'Мультифункциональный бот для discord',
+        description: `New Senpaii — проект мультифункционального, в том числе и развлекательного бота для Вашего сервера в Discord, основанный на желаниях и потребностях пользователей аудитории.
+
+        Созданный в 2019 году бот, продолжающий радовать своих пользователей и находящийся в активной разработке будет рад выполнять свои функции и на Вашем сервере.`,
         icon: 'https://cdn.discordapp.com/avatars/674942800894361611/0a36c777c40d995bb58ed2917463db97.webp?size=4096',
         buttonShare: [{ link: '', title: 'Купить скрипт бота' }, ''],
         date: '3/4/2021',
         bg: 'bg1.jpg',
-        color: '#f2d3a9',
+        color: '',
         platform: ['Github', 'Discord'],
-        button: [{ buttonName: 'Купить', buttonLink: '', buttonColor: '#52b36e' }]
+        button: [{ buttonName: '2900₽', buttonLink: './items/buy-1.html', buttonColor: '' }]
     },
     2: {
         name: 'Asami#3799',
@@ -17,9 +20,9 @@ const profile = {
         buttonShare: [{ link: '', title: 'Купить скрипт бота' }, ''],
         date: '7/05/2021',
         bg: 'bg1.jpg',
-        color: '#ffc0ed',
+        color: '',
         platform: ['Github', 'Discord'],
-        button: [{ buttonName: 'Купить', buttonLink: '', buttonColor: '#52b36e' }]
+        button: [{ buttonName: '899₽', buttonLink: './items/buy-2.html', buttonColor: '' }]
     },
     3: {
         name: 'Zipp Wickness',
@@ -28,19 +31,19 @@ const profile = {
         buttonShare: [{ link: 'https://youtube.com/playlist?list=PL2KqJq9LIGN7q1zGbuYIi5jXvtamjrseQ', title: 'Перейти на YouTube плейлист' }, 'https://youtube.com/playlist?list=PL2KqJq9LIGN7q1zGbuYIi5jXvtamjrseQ - YouTube\nhttps://soundcloud.com/valeriy-is-bro-vp/sets/2-1 - SoundCloud'],
         date: '23/4/2021',
         bg: 'bg2.jpg',
-        color: '#ab7fbc',
+        color: '',
         platform: ['YouTube', 'SoundCloud'],
-        button: [{ buttonName: 'Смотреть', buttonLink: 'https://youtube.com/playlist?list=PL2KqJq9LIGN7q1zGbuYIi5jXvtamjrseQ', buttonColor: '#fe6c58' }, { buttonName: 'Слушать', buttonLink: 'https://soundcloud.com/valeriy-is-bro-vp/sets/2-1', buttonColor: '#fe9433' }]
+        button: [{ buttonName: 'Смотреть', buttonLink: 'https://youtube.com/playlist?list=PL2KqJq9LIGN7q1zGbuYIi5jXvtamjrseQ', buttonColor: '' }, { buttonName: 'Слушать', buttonLink: 'https://soundcloud.com/valeriy-is-bro-vp/sets/2-1', buttonColor: '' }]
     }
 }
 
 
 let code = '';
-for (let i = 1; i <= Object.keys(profile).length; i++) {
+for (let i = 1; i <= Object.keys(profile).length - 1; i++) {
     code = code + `<tbody>
 <tr class="tr"
 style="box-shadow: inset 0px 0px 150px rgba(0,0,0,1); background: url(./img/bgs/${profile[i].bg}) no-repeat #343D46; background-size: cover; background-position: 0px -100px;">
-<td style="background: ${profile[i].color}; border: 5px solid ${profile[i].color};" class=" numSection t3">
+<td style="background: ${profile[i].color !== '' ? profile[i].color : profile.defColor}; border: 5px solid ${profile[i].color !== '' ? profile[i].color : profile.defColor};" class=" numSection t3">
 
     <span name="place" value="528509008022208513">${i}</span>
 `;
@@ -52,11 +55,11 @@ style="box-shadow: inset 0px 0px 150px rgba(0,0,0,1); background: url(./img/bgs/
 
     code = code + `
 </td >
-        <td style="border: 5px solid ${profile[i].color};" class="serverAvatar t3">
+        <td style="border: 5px solid ${profile[i].color !== '' ? profile[i].color : profile.defColor};" class="serverAvatar t3">
             <img class="" src="${profile[i].icon}"
                 onerror="this.src='./img/avatars/avatar.gif'" alt="">
 </td>
-            <td style="border: 5px solid ${profile[i].color};" class="serverDesc t3">
+            <td style="border: 5px solid ${profile[i].color !== '' ? profile[i].color : profile.defColor};" class="serverDesc t3">
                 <div class="row">
                     <div class=" hideServerAvatar">
                         <img class="mobileServerAvatar"
@@ -71,7 +74,7 @@ style="box-shadow: inset 0px 0px 150px rgba(0,0,0,1); background: url(./img/bgs/
                         <p class="description">${profile[i].description}
                         </p>
 </td>
-                    <td style="border: 5px solid ${profile[i].color};" class="join t3"><span
+                    <td style="border: 5px solid ${profile[i].color !== '' ? profile[i].color : profile.defColor};" class="join t3"><span
                         class="rating all"><b>${profile[i].date}</b></span>
                     `;
     for (let but = 0; !!profile[i].button[but]; but++) {
@@ -79,7 +82,7 @@ style="box-shadow: inset 0px 0px 150px rgba(0,0,0,1); background: url(./img/bgs/
                     <a href="${profile[i].button[but].buttonLink}"
                         style="background: ${profile[i].button[but].buttonColor !== '' ? profile[i].button[but].buttonColor : profile[i].color};"
                         onmouseover="this.style.backgroundColor='${profile[i].button[but].buttonColor !== '' ? profile[i].button[but].buttonColor : profile[i].color} linear-gradient(-80deg, rgba(255, 255, 255, 0), rgba(255, 255, 255, .5), rgba(255, 255, 255, 0)) no-repeat !important';"
-                        onmouseout="this.style.backgroundColor='${profile[i].button[but].buttonColor !== '' ? profile[i].button[but].buttonColor : profile[i].color}';" class="boost t3"
+                        onmouseout="this.style.backgroundColor='${profile[i].button[but].buttonColor !== '' ? profile[i].button[but].buttonColor : profile[i].color}';" class="black boost"
                         target="_blank" rel="noopener" title="${profile[i].button[but].buttonName}"
                         tabindex="0">${profile[i].button[but].buttonName}</a>
                         `

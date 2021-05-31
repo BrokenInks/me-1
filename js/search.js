5f85a6c85a4604c4722784c7178508cd88ab92d3
ZippWitch-alt <78442160+ZippWitch-alt@users.noreply.github.com>
2021-05-31 13:49:03 +0300

    ф

--- js/search.js
+++ js/search.js
@@ -1,44 +1,60 @@
-function searchTab() {
-    // Объявить переменные
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("mySearch");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myMenu");
-    li = ul.getElementsByTagName("li");
-
-    // Перебирайте все элементы списка и скрывайте те, которые не соответствуют поисковому запросу
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
-
 function redirectUser() {
     let link = 'https://zippwitch-alt.github.io/zippp/'
-    var code = document.getElementById("code").value;
     let file;
+    var code = document.getElementById("code").value;
+    const regexps = {
+        'portfolio': /(портфолио)|(пф)|(portfolio)|(pf)/ui.test(code),
+        'news': /(новости)|(news)|(события)|(нововведения)/ui.test(code),
+        'more': /(подробнее)|(ещё)|(больше)|(more)/ui.test(code),
+        'index': /(главная)|(main)|(индекс)|(домашняя)|(home)/ui.test(code),
+        'game3000': /Hello, world!/.test(code)
+    }
 
     switch (true) {
-        case /(портфолио)|(пф)|(portfolio)|(pf)/ui.test(code):
+        case regexps['portfolio']:
             file = 'portfolio'
             break;
-        case /(новости)|(news)|(события)|(нововведения)/ui.test(code):
+        case regexps['news']:
             file = 'news'
             break;
-        case /(подробнее)|(ещё)|(больше)|(more)/ui.test(code):
+        case regexps['more']:
             file = 'more'
             break;
-        case /(главная)|(main)|(индекс)|(домашняя)|(home)/ui.test(code):
+        case regexps['index']:
             file = 'index'
             break;
-        case /Hello, world!/.test(code):
+        case regexps['game3000']:
             file = 'game3000'
             break;
     }
 
     window.location.href = file !== 'index' ? link + file + '.html' : link
 }
+
+function checkValid() {
+    document.getElementById("code").value = document.getElementById("code").value
+    var code = document.getElementById("code").value;
+    const regexps = {
+        'portfolio': /(портфолио)|(пф)|(portfolio)|(pf)/ui.test(code),
+        'news': /(новости)|(news)|(события)|(нововведения)/ui.test(code),
+        'more': /(подробнее)|(ещё)|(больше)|(more)/ui.test(code),
+        'index': /(главная)|(main)|(индекс)|(домашняя)|(home)/ui.test(code),
+        'game3000': /Hello, world!/.test(code)
+    }
+    let result = regexps['index'];
+
+    if (regexps['portfolio'] ||
+        regexps['news'] ||
+        regexps['more'] ||
+        regexps['index'] ||
+        regexps['game3000']) {
+        document.getElementById("code").style.color = "green"
+    } else {
+        document.getElementById("code").style.color = "#322a63a4"
+    }
+    console.log(result)
+
+
+}
+
+
